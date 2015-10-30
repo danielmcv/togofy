763307a4374f19a1b8836ee17caea270f870e397
cognatus <diegojmartinezz@hotmail.com>
2015-10-29 20:31:30 -0600

    Modelo en proceso

---
+++ restaurantes/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Datos(models.Model):
+	"""datos del restaurant"""
+	dueno = models.ForeignKey('usuarios.Restaurante')
+	nombre = models.CharField(max_length=30)
+	direccion = models.CharField(max_length=30)
+	email = models.EmailField(max_length=30)
+	imagen = models.ImageField()
+	telefono = models.CharField(max_length=10)
+	calificacion = models.CharField(max_length=10)
+	tipo = models.CharField(max_length=10)
+
+
+class Platillos(models.Model):
+	"""datos de platillos"""
+	restaurante = models.ForeignKey(Datos)
+	nombre = models.CharField(max_length=30)
+	imagen = models.ImageField()
+	calificacion = models.CharField(max_length=10)
+	tipo = models.CharField(max_length=10)
+	precio = models.IntegerField()
+
+class Cosas(models.Model):
+	"""datos de cosas"""
+	_id = models.ForeignKey('usuarios.Orden')
+	platillo = models.ForeignKey(Platillos)
+	precio = models.ForeignKey(Platillos)
+	cantidad = models.IntegerField()
--- usuarios/models.py
+++ usuarios/models.py
@@ -2,21 +2,38 @@ from django.db import models
 
 # Create your models here.
 
+tipos_usuarios = (
+	(CLIENTE, "cliente"),
+	(RESTAURANT, "restaurant"),
+)
+
+class Usuario(models.Model):
+	"""datos de los usuarios"""
+	nombre = models.CharField(max_length=30)
+	apellido = models.CharField(max_length=30)
+	email = models.EmailField(max_length=30, )
+	edad = models.CharField(max_length=2)
+	imagen = models.ImageField()
+	password = models.CharField(max_length=15)
+	telefono = models.CharField(max_length=10)
+	celular = models.CharField(max_length=10)
+	tipo = models.ChoiceField(opcion_chida1)
+
 class Cliente(models.Model):
-	"""datos de los clientes"""
-		first_name = models.CharField(max_length=30)
-		last_name = models.CharField(max_length=30)
-		email = models.EmailField(max_length=30)
-		edad = models.CharField(max_length=2)
-		imagen= models.ImageField()
+	"""datos de usuario cliente """
+	usuario_cliente = models.ForeignKey(Usuario)
+	pago = models.BooleanField(default=False)
 
 
 class Restaurante(models.Model):
 	"""datos de usuario restaurant """
-		name = models.CharField(max_length=30)
-		
+	usuario_restaurant = models.ForeignKey(Usuario)
+	servicio = models.BooleanField(default=False)
 
-	
-		
-		
-
+class Orden(models.Model):
+	"""datos de  orden"""
+	_id = models.CharField(max_length=30)
+	cliente = models.ForeignKey(Cliente)
+	lugar_entrega = models.CharField(max_length=30)
+	precio_total = models.IntegerField()
+	tipo = models.BooleanField(default=False)
